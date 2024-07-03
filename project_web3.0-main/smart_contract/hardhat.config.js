acb6deedbed937ec4871c39a425b66c4d68090e8
USER1995 <143902610+Userfrom1995@users.noreply.github.com>
2024-07-03 19:24:11 +0000

    Tutorial Complete

--- project_web3.0-main/smart_contract/hardhat.config.js
+++ project_web3.0-main/smart_contract/hardhat.config.js
@@ -1,9 +1,10 @@
 require('@nomiclabs/hardhat-waffle');
+require('@nomicfoundation/hardhat-ignition');
 
 module.exports = {
   solidity: '0.8.24',
   networks: {
-    ropsten: {
+    fantom: {
       url: '',
       accounts: [''],
     },
